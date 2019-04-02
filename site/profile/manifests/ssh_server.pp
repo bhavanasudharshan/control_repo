bed4aa27694e798833e3fbf10af28f9f10de7c8d
bhavanasudharshan <40013803+bhavanasudharshan@users.noreply.github.com>
2019-04-02 11:40:15 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,11 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCdnK+f4FPE0/rTgJ9JqxOwgMLjzkrM3A8│
-7ltffUpmB8Oks1B/wlqkIzvx4z551Kj+HDOliWy577yp0YFVFWBRTbnVcxopW35Cft6TbER│
-w8fzvf+8DvvYrCSqunmSgyx2I48SfBo7KKDpF4bhk2vidnJydfppsLHFkqZU1R0yOZplCaF│
-9SCWSunbn09KnBlrv3Fu3CQLGgxZcTNJLTFOeTkldv2JQCg6IhczwU1CTRdcbEGVPwW/bI4│
-0aMlVz6TALFYM0Vc1irUa2t9J5N2nEl6/xWw7BO+RZFO5lk/Egqi+8GtwXDXGKNNtV/3d73│
-Iks/0VzkNZmuxj9JTYQ6sMYlt',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCdnK+f4FPE0/rTgJ9JqxOwgMLjzkrM3A8│7ltffUpmB8Oks1B/wlqkIzvx4z551Kj+HDOliWy577yp0YFVFWBRTbnVcxopW35Cft6TbER│w8fzvf+8DvvYrCSqunmSgyx2I48SfBo7KKDpF4bhk2vidnJydfppsLHFkqZU1R0yOZplCaF│9SCWSunbn09KnBlrv3Fu3CQLGgxZcTNJLTFOeTkldv2JQCg6IhczwU1CTRdcbEGVPwW/bI4│0aMlVz6TALFYM0Vc1irUa2t9J5N2nEl6/xWw7BO+RZFO5lk/Egqi+8GtwXDXGKNNtV/3d73│Iks/0VzkNZmuxj9JTYQ6sMYlt',
   }  
 }
